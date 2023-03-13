0d52d7757bc0b68b9651dd4dbab2262648afa96b
Arshadpitafi <127642608+Arshadpitafi@users.noreply.github.com>
2023-03-13 15:27:09 +0500

    Update lala1.py

--- lala1.py
+++ lala1.py
@@ -372,9 +372,9 @@ logo = ("""\033[1;32m
                                                  
  ••••••••••••••••••••••••••••••••••••••••••••••••••\033[1;33m
   \033[1;32mOwner  :  ARSHAD PITAFI
-  \033[1;32mGithub: KING404
-  \033[1;33mStatus  :  Paid
-\033[1;34mWorking : All Device Working Bro
+  \033[1;32mGithub : KING404
+  \033[1;33mStatus :  Paid
+\033[1;34mWorking  : All Device Working Bro
  \033[1;37mVersion : 0.2  
  ••••••••••••••••••••••••••••••••••••••••••••••••••\033[1;34m""")
 
